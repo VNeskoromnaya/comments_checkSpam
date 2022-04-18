91ad8f1c37f1da6b01e0e838776c262b3941dafa
Valeriia Neskoromnaya <81985957+VNeskoromnaya@users.noreply.github.com>
2022-04-18 16:19:00 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const textArea = document.getElementById('comment');
+const messagesList = document.getElementById('field');
+const btn = document.getElementById('btn');
+
+function sendComment() {
+    let comment = textArea.value; // получаю содержимое 
+    let newMessage = document.createElement('div');
+    newMessage.className = 'completed';
+    newMessage.innerHTML = comment.toLowerCase().replace(/Viagra/gi, '***').replace(/XXX/gi, '***');
+    messagesList.appendChild(newMessage);
+    textArea.value = ''; // очищение textArea
+    textArea.focus();
+};
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    sendComment();
+    
+});
+
+
+// Необходимо реализовать отображение и добавление сообщений, 
+// а также функцию checkSpam(str), заменяющую 'viagra' или 'XXX' на ***
+// Функция должна быть нечувствительна к регистру:
